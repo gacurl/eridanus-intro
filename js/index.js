215449772d74b1963a6b50d5ea857b56ca2ae11a
gacurl <greg.a.curl@gmail.com>
2023-02-03 16:51:37 -0500

    add Skills section using JS

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
-let today = new Date();
-let thisYear = today.getFullYear();
-// console.log(thisYear);
+// Footer
 const fullName = "Greg Curl";
 const footer = document.querySelector("footer");
+
+let today = new Date();
+let thisYear = today.getFullYear();
 let copyright = document.createElement("p");
 
-copyright.innerHTML = `${fullName} ${thisYear} & copy`;
-// console.log(copyright);
+copyright.innerHTML = `${fullName} ${thisYear} &copy`;
 footer.appendChild(copyright)
 
+// Skills section
+const skills = ["HTML", "CSS", "Ruby", "Rails", "Python"]
+
+let skillsSection = document.getElementById("skills");
+
+let skillsList = skillsSection.querySelector("ul")
+// console.log(skillsList);
+for (let i = 0; i < skills.length; i++) {
+    let skill = document.createElement("li");
+    skill.innerHTML = `${skills[i]}`;
+    skillsList.appendChild(skill);
+
+
+    // const para = document.createElement("p");
+    // para.innerText = "This is a paragraph";
+    // document.body.appendChild(para);
+}
