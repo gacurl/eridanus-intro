a829ad6a27a764a39196f2d8334e9f1c05adb5cc
gacurl <greg.a.curl@gmail.com>
2023-02-05 08:32:56 -0500

    add removeButton and functionality - needs testing

--- js/index.js
+++ js/index.js
@@ -37,9 +37,20 @@ document.addEventListener("submit", function(event) {
     const messageSection = document.getElementById("messages")
     const messageList = messageSection.querySelector("ul")
     const newMessage = document.createElement("li");
-    newMessage.innerHTML = `<a href="mailto:${email}"> ${name}</a>   <span>${message}</span>`;
+    const removeButton = document.createElement("button")
+
+    newMessage.innerHTML = `<a href="mailto:${email}"> ${name}</a> wrote: <span>${message}</span>  `;
+    removeButton.innerHTML = `remove`;
+    // Remove message display button
+    document.addEventListener("click", function(event) {
+        const entry = newMessage //may need to specify only newMessage vs the entire list
+        entry.remove();
+        // I think the parentNode is #messages
+    });
+    // console.log("Entry outside the click event" + entry);
+    newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
-    // console.log(newMessage)
+    // console.log(removeButton)
     messageForm[0].reset();
 });
 
