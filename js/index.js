3881d7e3b104d585945d8eebd9c48b323c6a8590
gacurl <greg.a.curl@gmail.com>
2023-02-05 06:54:35 -0500

    add message Form and start message dispkay functionality

--- js/index.js
+++ js/index.js
@@ -8,12 +8,11 @@ let copyright = document.createElement("p");
 
 copyright.innerHTML = ` &copy ${fullName} ${thisYear}`;
 footer.appendChild(copyright)
+// end Footer
 
 // Skills section
 const skills = ["HTML", "CSS", "Ruby", "Rails", "Python"]
-
-let skillsSection = document.getElementById("skills");
-
+const skillsSection = document.getElementById("skills");
 let skillsList = skillsSection.querySelector("ul")
 // console.log(skillsList);
 for (let i = 0; i < skills.length; i++) {
@@ -21,3 +20,27 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerHTML = `${skills[i]}`;
     skillsList.appendChild(skill);
 }
+// end Skills
+
+// Messages
+const messageForm = document.getElementsByName("leave_message")
+
+document.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = event.target.name.value.trim();
+    const email = event.target.email.value.trim();
+    const message = event.target.message.value;
+    // not certain reset is clean, but it worked through testing
+    // need test strategy for resetting a nodelist
+
+    //Start message display
+    const messageSection = document.getElementById("messages")
+    const messageList = messageSection.querySelector("ul")
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${email}"> ${name}</a>   <span>${message}</span>`;
+    messageList.appendChild(newMessage);
+    // console.log(newMessage)
+    messageForm[0].reset();
+});
+
+// End Messages
