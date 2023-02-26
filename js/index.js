deaae074823ebdbb56adfb7a2fcd3fcdc3e380ba
gacurl <greg.a.curl@gmail.com>
2023-02-26 09:25:33 -0500

    add Projects link in header

--- js/index.js
+++ js/index.js
@@ -87,6 +87,16 @@ function renderProjectList(repositories) {
         projectList.appendChild(project)
     }
 };
+
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats', {
+    mode: 'cors'
+})
+.then(function (response) {
+    return response.json();
+})
+.then(function (response) {
+    console.log(response);
+});
 // End Fetch GH repos
 
 // Footer
